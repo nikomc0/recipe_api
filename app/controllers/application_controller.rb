a0a01249fc6acaff34e611335291514c19ce49e4
Dominick Del Bosque <dominick@theosfp.com>
2021-10-06 14:28:02 -0600

    adds the ability to create new recipe

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 require 'sinatra'
 require 'json'
-
+require 'pry-byebug'
 class ApplicationController < Sinatra::Base
 	get '/' do
 		code = "<h3>API Grocery Helper</h3>"
@@ -14,4 +14,18 @@ class ApplicationController < Sinatra::Base
 		return @recipes.to_json
 		# erb :_recipes_list, :layout => :index
 	end
+
+	# CREATEs new recipe
+	post '/recipe' do
+		params = JSON.parse(request.body.read)
+
+		@recipe = Recipe.new
+		@recipe.name = params["name"]
+
+		if @recipe.save 
+			@recipe.to_json
+		else
+			p "Problem saving recipe."
+		end
+	end 
 end
