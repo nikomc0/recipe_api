185c5cb2f84778165c2c9778ffb88f0759ad2595
Dominick Del Bosque <6655845+nikomc0@users.noreply.github.com>
2024-02-02 14:48:52 -0700

    Configure rspec for testing (#3)
    * Add rspec to gemfile
    
    * Configured RSpec for proper testing

--- config/environment.rb
+++ config/environment.rb
@@ -4,13 +4,29 @@ require 'pry'
 require 'yaml'
 require 'dotenv'
 
-Bundler.require
+Bundler.require(:default)
+# Bundler.require(Sinatra::Base.environment)
+
 Dotenv.load('.env')
+
+set :environment, ENV['APP_ENV']
 
 configure :development do
     set :database, {
         adapter: ENV['PG_ADAPTER'],
         database: ENV['PG_DATABASE'],
+        pool: ENV['PG_POOL'],
+        host: ENV['PG_HOST'],
+        port: ENV['PG_PORT'],
+        username: ENV['PG_USERNAME'],
+        password: ENV['PG_PASSWORD']
+    }
+end
+
+configure :test do
+    set :database, {
+        adapter: ENV['PG_ADAPTER'],
+        database: ENV['PG_DATABASE_TEST'],
         pool: ENV['PG_POOL'],
         host: ENV['PG_HOST'],
         port: ENV['PG_PORT'],
--- db/schema.rb
+++ db/schema.rb
@@ -2,15 +2,16 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# This file is the source Rails uses to define your schema when running `bin/rails
-# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# This file is the source Rails uses to define your schema when running `rails
+# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
 # be faster and is potentially less error prone than running all of your
 # migrations from scratch. Old migrations may fail to apply correctly if those
 # migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2021_10_05_193546) do
+ActiveRecord::Schema.define(version: 2021_10_05_193546) do
+
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,8 +20,8 @@ ActiveRecord::Schema[7.1].define(version: 2021_10_05_193546) do
     t.string "brand"
     t.string "store"
     t.string "category"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
     t.index ["name"], name: "index_ingredients_on_name", unique: true
   end
 
@@ -35,8 +36,8 @@ ActiveRecord::Schema[7.1].define(version: 2021_10_05_193546) do
 
   create_table "recipes", force: :cascade do |t|
     t.string "name", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
